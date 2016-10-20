77a6ed92aea4869fde5dac347798f2de8254cbc4
Giuseppe Stanghellini <giuseppe.stanghellini@gmail.com>
2016-10-20 16:35:09 +0200

    Intial commit

---
+++ EXAMPLES/extract_navigation.sh
@@ -0,0 +1,14 @@
+echo ""
+echo "The following command extract coordinates from a segy file"
+echo "Executing:"
+echo "segy-change -f DATA/TEST_isa.sgy -dump_xy SOURCE"
+pause "continue."
+segy-change -f DATA/TEST_isa.sgy -dump_xy SOURCE
+echo ""
+echo "If the coordinates are in arc seconds they can be converted in degrees with"
+echo "Executing:"
+echo "segy-change -f DATA/6_channel.seg -dump_xy SOURCE | awk '{printf(\"%d %d %d %lf %lf\\n\", \$1, \$2, \$2, \$4/3600.00, \$5/3600.0);}'"
+pause "continue."
+segy-change -f DATA/6_channel.seg -dump_xy SOURCE | awk '{printf("%d %d %d %lf %lf\n", $1, $2, $2, $4/3600.00, $5/3600.0);}'
+
+
